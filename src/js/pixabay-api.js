079d14b497c39845efdcc7bc0c37a4df751b94d1
Vadim13245 <159133092+Vadim13245@users.noreply.github.com>
2024-04-06 21:32:03 +0300

    5yui

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,15 @@
+import { galleryList } from '../main';
+import { query } from '../main';
+
+export function fetchImages(query) {
+  query = encodeURIComponent(query);
+  galleryList.innerHTML = `<div class="loader"></div>`;
+  return fetch(
+    `https://pixabay.com/api/?key=42609290-856768105ab9e79485c69bf61&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`
+  ).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
+}
